896a43afe7067d8f1121cb924f59f78a1742ba82
Rory Murray <rory.christian.murray@gmail.com>
2019-10-03 09:10:42 -0400

    Set-up day one lesson

---
+++ lesson-one/block.js
@@ -0,0 +1,42 @@
+const SHA256 = require("crypto-js/sha256");
+
+class Block {
+  constructor(timestamp, prevHash, data, hash) {
+    this.timestamp = timestamp;
+    this.prevHash = prevHash;
+    this.data = data;
+    this.hash = hash;
+  }
+
+  toString() {
+    return `Block -
+                  Timestamp: ${this.timestamp}
+                  PrevHash : ${this.prevHash.substring(0, 10)}
+                  Data     : ${this.data}
+                  Hash     : ${this.hash.substring(0, 10)}
+        `;
+  }
+
+  static genesis() {
+    const timestamp = "Genesis Time";
+    const prevHash = "----------";
+    const data = [];
+    const hash = Block.hash(timestamp, prevHash, data);
+
+    return new this(timestamp, prevHash, data, hash);
+  }
+
+  static mineBlock(prevBlock, data) {
+    const timestamp = Date.now();
+    const prevHash = prevBlock.hash;
+    const hash = Block.hash(timestamp, prevHash, data);
+
+    return new this(timestamp, prevHash, data, hash);
+  }
+
+  static hash(timestamp, prevHash, data) {
+    return SHA256(`${timestamp}${prevHash}${data}`).toString();
+  }
+}
+
+module.exports = Block;
